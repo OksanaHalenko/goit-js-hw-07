089ec9af40d37aed65d5c337180c6e3a29ebc22f
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-03-12 11:38:49 +0100

    add task 4-6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit",formSubmit);
+function formSubmit(event) {
+    event.preventDefault();
+    const userEmail = event.target.elements.email.value.trim();
+    const userPassword = event.target.elements.password.value.trim();
+    if (userEmail !== "" & userPassword !== "") {
+        const user = {
+            email: userEmail,
+            password: userPassword
+        }
+        console.log(user);
+        event.target.reset();
+    } else {
+        alert("All form fields must be filled in")
+    }
 
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputNumber = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]")
+const boxes = document.querySelector("#boxes");
+btnCreate.addEventListener("click", checkCondition);
+
+function checkCondition() {
+  if (inputNumber.value < 1 || inputNumber.value > 100) {
+    inputNumber.value = "";
+    return;
+  } else {
+    boxes.innerHTML = "";
+    createBoxes(inputNumber.value);
+  }
+  inputNumber.value = "";
+}
+function createBoxes(amount) {
+  
+ const boxesCollection = [];
+    for (let i = 1; i <= amount; i++) {
+      const box = document.createElement("div");
+      box.style.width = `${30 + (i - 1) * 10}px`;
+      box.style.height = `${30 + (i - 1) * 10}px`;
+      box.style.backgroundColor = getRandomHexColor();
+      
+      boxesCollection.push(box);
+    }
+     boxes.append(...boxesCollection);
+}
+btnDestroy.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
